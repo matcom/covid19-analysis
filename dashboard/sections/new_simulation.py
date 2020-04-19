acbdd3f56639f1b51a9dd8af503e55d89dcf5a75
Alejandro Piad <alepiad@gmail.com>
2020-04-19 21:21:07 +0200

    Update simulation
    Co-authored-by: Suilan Estevez-Velarde <sestevez@matcom.uh.cu>

--- dashboard/sections/new_simulation.py
+++ dashboard/sections/new_simulation.py
@@ -1,129 +1,173 @@
 import streamlit as st
 import altair as alt
 
+from ..data import raw_information
 from ..simulation import Simulation
+from ..simulation import compute_similarity
+from ..simulation import optimize_similarity
 
 
 def run(tr):
-    simulation = Simulation()
+    def post_process(df):
+        df["active"] = df["asymptomatic"] + df["symptomatic"]  # + df["Positive"]
+        df["confirmed"] = simulation["pop"] - df["susceptible"]
+        df["new_deaths"] = df["deaths"].diff().fillna(0)
+        df["letality"] = df["new_deaths"] / df["active"]
+        df["letality_smooth"] = df["letality"].rolling(10).mean().fillna(0)
+        return df
 
-    simulation.add_state("Susceptible")
-    simulation.add_state("Asymptomatic")
-    simulation.add_state("Symptomatic")
-    simulation.add_state("Recovered")
-    simulation.add_state("Dead")
-    simulation.add_state("Positive")
+    simulation = Simulation(after_run=post_process)
 
-    def alive(d):
-        return starting_population - d["Dead"]
+    simulation.add_state("susceptible")
+    simulation.add_state("asymptomatic")
+    simulation.add_state("symptomatic")
+    simulation.add_state("recovered")
+    simulation.add_state("deaths")
+    # simulation.add_state("Positive")
 
-    starting_population = st.sidebar.number_input("Population", 0, 100, 10) * 1000000
+    simulation["pop"] = (
+        st.sidebar.number_input("Potentially susceptible population", 0, 1 * 1000 ** 3, 10 * 1000 ** 2)
+    )
 
-    # people getting sick
+    # people getting active
     st.sidebar.markdown("### Infection dynamics")
 
-    n_meet = st.sidebar.slider("People meeting", 0, 100, 10)
-    n_test = st.sidebar.slider("Tests per day", 0, 10000, 1000)
-    p_n_test = st.sidebar.slider("Tests applied to Symptomatic", 0.0, 1.0, 0.8)
-    p_infect_asymptomatic = st.sidebar.slider(
+    simulation["n_meet"] = st.sidebar.slider("People meeting", 0, 100, 10)
+    simulation["n_test"] = st.sidebar.slider("Tests per day", 0, 10000, 1000)
+    simulation["p_n_test"] = st.sidebar.slider(
+        "Tests applied to symptomatic", 0.0, 1.0, 0.8
+    )
+    simulation["p_infect_asymptomatic"] = st.sidebar.slider(
         "Probability of infection from asymptomatic", 0.0, 1.0, 0.1
     )
-    p_infect_symptomatic = st.sidebar.slider(
+    simulation["p_infect_symptomatic"] = st.sidebar.slider(
         "Probability of infection from symptomatic", 0.0, 1.0, 0.2
     )
 
     simulation.add_transition(
-        "Susceptible",
-        "Asymptomatic",
-        lambda d: (
-            d["Asymptomatic"]
-            * n_meet
-            * p_infect_asymptomatic
-            * d["Susceptible"]
-            / alive(d)
-        ),
-    )
+        "susceptible",
+        "asymptomatic",
+        "asymptomatic * n_meet * p_infect_asymptomatic * susceptible / (pop - deaths)",
+    ),
 
     simulation.add_transition(
-        "Susceptible",
-        "Asymptomatic",
-        lambda d: (
-            d["Symptomatic"]
-            * n_meet
-            * p_infect_symptomatic
-            * d["Susceptible"]
-            / alive(d)
-        ),
-    )
+        "susceptible",
+        "asymptomatic",
+        "symptomatic * n_meet * p_infect_symptomatic * susceptible / (pop - deaths)",
+    ),
 
-    simulation.add_transition(
-        "Asymptomatic",
-        "Positive",
-        lambda d: (
-            min(
-                d["Asymptomatic"] * n_test * (1 - p_n_test) / alive(d),
-                d["Asymptomatic"] * (1 - (p_asympt_recover + p_symptoms)),
-            )
-        ),
-    )
+    # simulation.add_transition(
+    #     "asymptomatic",
+    #     "Positive",
+    #     "min(asymptomatic * n_test * (1 - p_n_test) / (pop - deaths), asymptomatic * (1 - (p_asympt_recover + p_symptoms)))",
+    # )
 
-    simulation.add_transition(
-        "Symptomatic",
-        "Positive",
-        lambda d: (
-            min(
-                d["Symptomatic"] * (1 - (p_sympt_recover + p_sympt_dead)),
-                n_test * p_n_test / 100,
-            )
-        ),
-    )
+    # simulation.add_transition(
+    #     "symptomatic",
+    #     "Positive",
+    #     "min(symptomatic * (1 - (p_sympt_recover + p_sympt_dead)), n_test * p_n_test)",
+    # )
 
     # people developing symptoms and disease
     st.sidebar.markdown("### Disease evolution")
 
-    p_symptoms = st.sidebar.slider(
+    simulation["p_symptoms"] = st.sidebar.slider(
         "Chance of developing symptoms (daily)", 0.0, 1.0, 0.1
     )
-    p_asympt_recover = st.sidebar.slider(
+    simulation["p_asympt_recover"] = st.sidebar.slider(
         "Chance of recovering for asymptomatic patients (daily)", 0.0, 1.0, 0.2
     )
-    p_sympt_dead = st.sidebar.slider(
+    simulation["p_sympt_dead"] = st.sidebar.slider(
         "Chance of dying for symptomatic patients (daily)", 0.0, 1.0, 0.05
     )
-    p_sympt_recover = st.sidebar.slider(
+    simulation["p_sympt_recover"] = st.sidebar.slider(
         "Chance of recovering for symptomatic patients (daily)", 0.0, 1.0, 0.1
     )
 
-    simulation.add_transition("Asymptomatic", "Symptomatic", p_symptoms)
-    simulation.add_transition("Asymptomatic", "Recovered", p_asympt_recover)
-    simulation.add_transition("Symptomatic", "Dead", p_sympt_dead)
-    simulation.add_transition("Symptomatic", "Recovered", p_sympt_recover)
-    simulation.add_transition("Positive", "Dead", p_sympt_dead)
-    simulation.add_transition("Positive", "Recovered", p_sympt_recover)
+    simulation.add_transition(
+        "asymptomatic", "symptomatic", "asymptomatic * p_symptoms"
+    )
+    simulation.add_transition(
+        "asymptomatic", "recovered", "asymptomatic * p_asympt_recover"
+    )
+    simulation.add_transition("symptomatic", "deaths", "symptomatic * p_sympt_dead")
+    simulation.add_transition(
+        "symptomatic", "recovered", "symptomatic * p_sympt_recover"
+    )
+    # simulation.add_transition("Positive", "deaths", "Positive * p_sympt_dead")
+    # simulation.add_transition("Positive", "recovered", "Positive * p_sympt_recover")
 
-    data = simulation.run(1000, Susceptible=starting_population, Asymptomatic=1).astype(
-        int
+    st.write("### Comparison with a real curve")
+
+    raw = raw_information()
+    countries = list(raw.keys())
+    country = st.selectbox("Select a country to compare with", countries, countries.index("Cuba"))
+
+    real = raw[country].set_index('date')
+
+    if st.checkbox("Show real country data"):
+        st.write(real)
+
+    columns = list(real.columns)
+    columns = st.multiselect("Columns to compare", columns, columns)
+
+    if st.checkbox("Find best parameters (slow)"):
+
+        parameters = {}
+
+        for param, value in simulation.parameters.items():
+            if isinstance(value, float):
+                min_value, max_value = st.slider(param, min_value=0.0, max_value=1.0, value=(0.0, 1.0))
+            elif isinstance(value, int) and value < 100:
+                min_value, max_value = st.slider(param, min_value=0, max_value=100, value=(0, 100))
+            else:
+                continue
+
+            parameters[param] = (min_value, max_value) #, (max_value - min_value) / 10)
+
+        if st.button("Run optimization"):
+            best_params = optimize_similarity(simulation, real, columns, parameters, susceptible=simulation['pop']-1, asymptomatic=1)
+            st.write(best_params)
+
+    error, series = compute_similarity(simulation, real, columns, susceptible=simulation['pop']-1, asymptomatic=1)
+
+    real_data_to_chart = real[columns].reset_index()
+    simulation_data_to_chart = series[columns]
+    simulation_data_to_chart['date'] = real_data_to_chart['date']
+
+    if st.checkbox("Show simulated data"):
+        st.write(simulation_data_to_chart)
+
+    st.write(f"#### Approximation error: {error:.2f}")
+
+    chart1 = alt.Chart(real_data_to_chart.melt("date")).mark_line().encode(
+        x='date',
+        y="value",
+        color='variable',
     )
 
-    data["Sick"] = data["Asymptomatic"] + data["Symptomatic"]
-    data["New dead"] = data["Dead"].diff().fillna(0)
-    data["Letality"] = data["New dead"] / data["Sick"]
-    data["Letality (smooth)"] = data["Letality"].rolling(10).mean()
-    data["Sum"] = (
-        data["Sick"]
-        + data["Dead"]
-        + data["Susceptible"]
-        + data["Recovered"]
-        + data["Positive"]
+    chart2 = alt.Chart(simulation_data_to_chart.melt("date")).mark_line(size=3).encode(
+        x='date',
+        y="value",
+        color='variable',
     )
-    data = data[data["Sick"] > 0]
+
+    st.altair_chart(chart1 + chart2, use_container_width=True)
+
+    st.sidebar.markdown("### Visualization")
+    st.write("### Simulated curve")
+
+    data = simulation.run(
+        200, susceptible=simulation["pop"] - 1, asymptomatic=1
+    ).astype(int)
+
+    if st.checkbox("Show simulation data"):
+        st.write(data)
 
     visualize = list(data.columns)
     visualize = st.multiselect(
-        "Columns to visualize", visualize, ["Asymptomatic", "Symptomatic", "Dead"]
+        "Columns to visualize", visualize, ["asymptomatic", "symptomatic", "deaths"]
     )
-
-    st.sidebar.markdown("### Visualization")
 
     scale = st.sidebar.selectbox("Scale", ["linear", "log"])
 
--- dashboard/simulation.py
+++ dashboard/simulation.py
@@ -1,23 +1,45 @@
 import pandas as pd
 import collections
 import graphviz
+import numpy as np
 
 
 class Simulation:
-    def __init__(self, **kwargs):
-        self.states = []
-        self.transitions = collections.defaultdict(list)
-        self.kwargs = kwargs
+    def __init__(self, after_run=None, **parameters):
+        self._states = []
+        self._transitions = collections.defaultdict(list)
+        self._parameters = parameters
+        self._after_run = after_run
+
+    @property
+    def states(self):
+        return list(self._states)
+
+    @property
+    def transitions(self):
+        return dict(self._transitions)
+
+    @property
+    def parameters(self):
+        return dict(self._parameters)
+
+    def __getitem__(self, name):
+        return self._parameters[name]
+
+    def __setitem__(self, name, value):
+        self._parameters[name] = value
 
     def add_state(self, state: str):
-        self.states.append(state)
+        self._states.append(state)
 
     def add_transition(self, from_state: str, to_state: str, transition):
-        self.transitions[(from_state, to_state)].append(transition)
+        self._transitions[(from_state, to_state)].append(transition)
 
     def run(self, n: int, **starting_values: dict):
         history = []
-        current_values = dict({state: starting_values.get(state, 0) for state in self.states}, **self.kwargs)
+        current_values = dict(
+            {state: starting_values.get(state, 0) for state in self._states}
+        )
 
         for i in range(n):
             history.append(current_values)
@@ -28,22 +50,27 @@ class Simulation:
                     if from_state == to_state:
                         continue
 
-                    for tr in self.transitions[(from_state, to_state)]:
+                    for tr in self._transitions[(from_state, to_state)]:
                         if callable(tr):
                             move = tr(current_values)
                         elif isinstance(tr, str):
-                            move = eval(tr, globals(), current_values)
+                            move = eval(tr, self.parameters, current_values)
                         else:
                             move = tr * current_values[from_state]
 
-                        move = min(move, current_values[from_state])
+                        move = max(0, min(move, new_values[from_state]))
 
                         new_values[from_state] -= move
                         new_values[to_state] += move
 
             current_values = new_values
 
-        return pd.DataFrame(history)
+        history = pd.DataFrame(history)
+
+        if self._after_run:
+            history = self._after_run(history)
+
+        return history
 
     def graph(self):
         graph = graphviz.Digraph()
@@ -51,7 +78,7 @@ class Simulation:
         for state in self.states:
             graph.node(state)
 
-        for (u,v), l in self.transitions.items():
+        for (u, v), l in self.transitions.items():
             for k in l:
                 if isinstance(k, float):
                     graph.edge(u, v, label="%.3f" % k)
@@ -63,15 +90,52 @@ class Simulation:
         return graph
 
 
-def simulate_trayectories(history: pd.DataFrame, start:str, n_simulations:int=1000):
-    raise NotImplementedError()
+def compute_similarity(
+    simulation: Simulation, real: pd.DataFrame, columns: list, **starting_values
+):
+    simulated = simulation.run(len(real), **starting_values)
+    errors = 0
 
-    steps = len(history)
-    trayectories = []
+    for c in columns:
+        errors += np.linalg.norm(simulated[c].values - real[c].values)
 
-    for _ in range(n_simulations):
-        trayectory = []
-        current_state = start
+    return errors / len(columns), simulated
 
-        for i in range(steps):
-            mass_per_state = None
+
+def optimize_similarity(
+    simulation, real, columns: list, parameter_ranges: dict, **starting_values
+):
+    pass
+    """
+    Minimiza el valor de similaridad(error de similaridad) probando con todos los posibles valores(parameter_ranges)
+    """
+    from scipy.optimize import differential_evolution
+
+    # poner en formato para pasarle los valores de los parámetros al optimizados de scipy
+    rranges = []
+    names_parameters = []
+    for i, v in parameter_ranges.items():
+        names_parameters.append(i)
+        rranges.append(v)
+    rranges = tuple(rranges)
+
+    # función a optimizar
+    def f(z):
+        for name, x in zip(names_parameters, z):
+            simulation[name] = x
+
+        sim, series = compute_similarity(simulation, real, columns, **starting_values)
+        print(f"Computing similarity for {z}={sim}")
+
+    # llamando al optimizador
+    resbrute = differential_evolution(f, rranges)
+
+    gm = resbrute.x  # global minimum
+    f_gm = resbrute.fun  # # function value at global minimum
+
+    # devolviendo parametros con el mejor valor encontrado
+    result = {}
+    for i, name in enumerate(names_parameters):
+        result[name] = gm[i]
+
+    return result
