c6a660b6f35f36cd0956b016e802f94eabf4fc3b
Alejandro Piad <alepiad@gmail.com>
2020-05-04 15:22:14 +0200

    Refactor

--- simulation.py
+++ simulation.py
@@ -13,65 +13,66 @@ class StatePerson(Enum):
     Iv = 5
     A = 6
     R = 7
-    H = 8 
+    H = 8
     D = 9
 
-#método de tranmisión espacial, teniendo en cuenta la localidad
+
+# método de tranmisión espacial, teniendo en cuenta la localidad
 def spacial_transmition(regions, social, status, distance, parameters):
     """
-    Método de tranmisión espacial, teniendo en cuenta la localidad
-    regions: regiones consideradas en la simulación.
-    social: es el conjunto de grafos que describen las redes en cada región.
-    status: contiene característucas y el estado de salud en cada región.
-    distance: almacena la distancia entre cada par de regiones.
-    parameters: parameters del modelo epidemiológico para cada individuo.
+    Método de tranmisión espacial, teniendo en cuenta la localidad.
 
-    output: es el estado actualizado de cada persona 
+    Args:
+        - regions: regiones consideradas en la simulación.
+        - social: es el conjunto de grafos que describen las redes en cada región.
+        - status: contiene característucas y el estado de salud en cada región.
+        - distance: almacena la distancia entre cada par de regiones.
+        - parameters: parameters del modelo epidemiológico para cada individuo.
+        - output: es el estado actualizado de cada persona.
     """
-    #cantidad de días(steps) que dura la simulación
+    # cantidad de días(steps) que dura la simulación
     simulation_time = 30
 
     # por cada paso de la simulación
-    for i in simulation_time:
-        #por cada región
+    for i in range(simulation_time):
+        # por cada región
         for region in regions:
-            #por cada persona
+            # por cada persona
             for ind in individuals:
-                #actualizar estado de la persona
+                # actualizar estado de la persona
                 ind.next_step()
 
-            #movimientos
+            # movimientos
             for n_region in regions:
                 if n_region != region:
-                    #calcular personas que se mueven de una region a otras
+                    # calcular personas que se mueven de una region a otras
                     pass
-        
+
 
 class Person:
-
     def __init__(self, region, age):
-        """Crea una nueva persona que por defecto está en el estado de suseptible al virus
+        """Crea una nueva persona que por defecto está en el estado de suseptible al virus.
         """
         self.state = StatePerson.S
         self.next_state = None
         self.steps_remaining = None
-        #TODO: llamar método de estado inicial
+        # TODO: llamar método de estado inicial
 
-        #la persona conoce la region a la que pertenece
+        # la persona conoce la region a la que pertenece
         self.region = region
         self.age = age
-        self. health_conditions = None
-    
+        self.health_conditions = None
+
     def next_step(self, region):
         """Ejecuta un step de tiempo para una persona.
         """
-        #actualizando la region
+        # actualizando la region
         self.region = region
 
         if self.steps_remaining == 0:
-            #actualizar state
+            # actualizar state
             self.state = self.next_state
-            #llamar al método del nuevo estado para definir tiempo y next_step 
+            # llamar al método del nuevo estado para definir tiempo y next_step
             if self.state == StatePerson.S:
                 state, time = self.p_suseptible()
                 self.next_state = state
@@ -113,13 +114,13 @@ class Person:
                 self.next_state = state
                 self.steps_remaining = time
 
-        else: 
-            #decrementar los steps que faltan para cambiar de estado
+        else:
+            # decrementar los steps que faltan para cambiar de estado
             self.steps_remaining = self.steps_remaining - 1
 
     # Funciones de que pasa en cada estado para cada persona
-    # debe devolver el tiempo que la persona va estar en ese estado y 
-    #a que estado le toca pasar en el futuro.
+    # debe devolver el tiempo que la persona va estar en ese estado y
+    # a que estado le toca pasar en el futuro.
 
     def p_suseptible(self):
         pass
@@ -151,12 +152,12 @@ class Person:
     def p_death(self):
         pass
 
-class Region:
 
+class Region:
     def __init__(self, population):
         self.recovered = 0
         self.population = population
-        self.death = 
+        self.death = 0
 
     @property
     def population(self):
@@ -170,17 +171,16 @@ class Region:
 @st.cache
 def load_interaction_estimates():
     # no funciona, error "Could not find a version that satisfies the requirement xldr"
-    df = pd.read_excel("/src/data/contact_matrices_152_countries/MUestimates_all_locations_1.xlsx", sheet_name=None)
+    df = pd.read_excel(
+        "/src/data/contact_matrices_152_countries/MUestimates_all_locations_1.xlsx",
+        sheet_name=None,
+    )
 
     return df
 
 
 def main():
     st.title("Simulación de la epidemia")
-
-    interaction_estimates = load_interaction_estimates()
-
-    st.write(interaction_estimates)
 
 
 if __name__ == "__main__":
