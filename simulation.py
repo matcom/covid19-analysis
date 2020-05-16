b27219a76e26ae51af15371f2a0d94bfc724db80
Alejandro Piad <alepiad@gmail.com>
2020-05-16 09:28:28 +0200

    Estimar las probabilidades de transición

--- simulation.py
+++ simulation.py
@@ -3,14 +3,75 @@ import pandas as pd
 import random
 import time
 import altair as alt
+import numpy as np
+import collections
 
-from typing import List
+from typing import List, Dict
 from enum import Enum
 
 
 @st.cache
 def load_disease_transition() -> pd.DataFrame:
     return pd.read_csv("./data/disease_transitions.csv")
+
+
+class TransitionEstimator:
+    def __init__(self):
+        self.data = load_disease_transition()
+        self.states = list(set(self.data['StateFrom']))
+        self._state_data = {}
+
+        for s in self.states:
+            self._state_data[s] = self.data[self.data["StateFrom"] == s]
+
+    def transition(self, from_state, age, sex):
+        """Computa las probabilidades de transición del estado `from_state` para una 
+        persona con edad `age` y sexo `sex.
+        """
+
+        # De todos los datos que tenemos, vamos a ordernarlos por diferencia absoluta
+        # con la edad deseada, y vamos cogiendo filas hasta acumular al menos 50
+        # mediciones. 
+        evidence = self.data[(self.data['StateFrom'] == from_state) & (self.data['Sex'] == sex)]
+        evidence['AgeDiff'] = (evidence['Age'] - age).abs()
+        evidence = evidence.sort_values(['AgeDiff', 'Count']).copy()
+        evidence['CountCumul'] = evidence['Count'].cumsum()
+        min_required_evidence = evidence[evidence['CountCumul'] >= 50]['CountCumul'].min()
+        evidence = evidence[evidence['CountCumul'] <= min_required_evidence]
+
+        # Ahora volvemos a agrupar por estado y recalculamos la media y varianza
+        state_to = collections.defaultdict(lambda: dict(count=0, mean=0, std=0))
+
+        for i, row in evidence.iterrows():
+            d = state_to[row['StateTo']]
+            d['count'] += row['Count']
+            d['mean'] += row['Count'] * row['MeanDays']
+            d['std'] += row['Count'] * row['StdDays']
+            d['state'] = row['StateTo']
+
+        for v in state_to.values():
+            v['mean'] /= v['count']
+            v['std'] /= v['count']
+
+        # Finalmente tenemos un dataframe con forma
+        # `count | mean | std | state`
+        # con una entrada por cada estado
+        return pd.DataFrame(state_to.values()).sort_values('count')
+
+
+TRANSITIONS = TransitionEstimator()
+
+
+@st.cache
+def load_interaction_estimates():
+    df: pd.DataFrame = pd.read_csv(
+        "./data/contact_matrices_152_countries/nicaragua.csv",
+        header=None,
+        names=[i for i in range(5, 85, 5)]
+    )
+    df['age'] = [i for i in range(5, 85, 5)]
+
+    return df.set_index("age").to_dict()
 
 
 class StatePerson:
@@ -59,7 +120,7 @@ def spatial_transmision(regions, social, status, distance, parameters):
         # por cada región
         for region in regions:
             # por cada persona
-            for ind in region.individuals:
+            for ind in region:
                 # actualizar estado de la persona
                 ind.next_step()
                 if ind.is_infectious:
@@ -100,17 +161,23 @@ def compute_spread(ind, social, status):
     connections = eval_connections(social, ind)
 
     for other in connections:
-        if other.is_infectious:
+        if other.state != StatePerson.S:
             continue
 
         if eval_infections(ind):
-            other.state = StatePerson.Lp
+            other.set_state(StatePerson.Lp)
 
 
-def eval_connections(social, person) -> List["Person"]:
+def eval_connections(social: Dict[int, Dict[int, float]], person: "Person") -> List["Person"]:
     """Devuelve las conexiones que tuvo una persona en un step de la simulación.
     """
-    pass
+    other_ages = social[person.age]
+
+    for age, lam in other_ages.items():
+        people = np.random.poisson(lam)
+
+        for i in range(people):
+            yield person.region.spawn(age)
 
 
 def eval_infections(person) -> bool:
@@ -118,28 +185,28 @@ def eval_infections(person) -> bool:
 
        En general depende del estado en el que se encuentra person y las probabilidades de ese estado
     """
-    pass
+    return True
 
 
 class Person:
-    def __init__(self, region, age):
+    total = 0
+
+    def __init__(self, region, age, sex):
         """Crea una nueva persona que por defecto está en el estado de suseptible al virus.
         """
+        Person.total += 1
         self.state = StatePerson.S
         self.next_state = None
         self.steps_remaining = None
         self.is_infectious = None
         # llamar método de estado inicial
-        self.initialize_person()
+        self.set_state(StatePerson.S)
 
         # la persona conoce la region a la que pertenece
         self.region = region
         self.age = age
+        self.sex = sex
         self.health_conditions = None
-
-    def initialize_person(self):
-        self.next_state = StatePerson.S
-        self.steps_remaining = 0
 
     def next_step(self):
         """Ejecuta un step de tiempo para una persona.
@@ -153,29 +220,17 @@ class Person:
             #     self.next_state = state
             #     self.steps_remaining = time
             if self.state == StatePerson.Ls:
-                state, time = self.p_latent_sintoms()
-                self.next_state = state
-                self.steps_remaining = time
+                self.p_latent_sintoms()
             elif self.state == StatePerson.Lp:
-                state, time = self.p_latent()
-                self.next_state = state
-                self.steps_remaining = time
+                self.p_latent()
             elif self.state == StatePerson.Ip:
-                state, time = self.p_infect()
-                self.next_state = state
-                self.steps_remaining = time
+                self.p_infect()
             elif self.state == StatePerson.Is:
-                state, time = self.p_infect_sitoms()
-                self.next_state = state
-                self.steps_remaining = time
+                self.p_infect_sitoms()
             elif self.state == StatePerson.Iv:
-                state, time = self.p_infect_sintom_antiviral()
-                self.next_state = state
-                self.steps_remaining = time
+                self.p_infect_sintom_antiviral()
             elif self.state == StatePerson.A:
-                state, time = self.p_asintomatic()
-                self.next_state = state
-                self.steps_remaining = time
+                self.p_asintomatic()
             else:
                 return False
             # en los estados restantes no hay transiciones
@@ -185,64 +240,69 @@ class Person:
         
         return True
 
+    def __repr__(self):
+        return f"Person(age={self.age}, sex={self.sex}, state={self.state}, steps_remaining={self.steps_remaining})"
+
     # Funciones de que pasa en cada estado para cada persona
     # debe devolver el tiempo que la persona va estar en ese estado y 
     # a que estado le toca pasar en el futuro.
 
+    def set_state(self, state):
+        self.next_state = state
+        self.steps_remaining = 0
+        self.next_step()
+
     def _evaluate_transition(self):
         """Computa a qué estado pasar dado el estado actual y los valores de la tabla.
         """
-        df = load_disease_transition()
-        # calcular el grupo de edad al que pertenece
-        age_group = min(df['Age'], key=lambda age: age >= self.age)
-        # quedarse con los valores del estado y edad del individuo actual
-        df = df[(df['Age'] == age_group) & (df["StateFrom"] == str(self.state))]
+        df = TRANSITIONS.transition(self.state, self.age, self.sex)
+        st.write(df)
         # calcular el estado de transición y el tiempo
-        to_state = random.choices(df['StateTo'].values, weights=df['Chance'].values, k=1)[0]
-        state_data = df.set_index('StateTo').to_dict('index')[to_state]
-        time = random.normalvariate(state_data['MeanDays'], state_data['StdDays'])
+        to_state = random.choices(df['state'].values, weights=df['count'].values, k=1)[0]
+        state_data = df.set_index('state').to_dict('index')[to_state]
+        time = random.normalvariate(state_data['mean'], state_data['std'])
 
         return to_state, int(time)
 
     def p_suseptible(self):
         self.is_infectious = False
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_latent_sintoms(self):
         self.is_infectious = True
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_latent(self):
-        self.is_infectious = True
-        return self._evaluate_transition()
+        self.is_infectious = False
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_infect(self):
         self.is_infectious = True
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_infect_sitoms(self):
         self.is_infectious = True
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_infect_sintom_antiviral(self):
         self.is_infectious = True
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_asintomatic(self):
         self.is_infectious = True
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_recovered(self):
         self.is_infectious = False
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_hospitalized(self):
-        self.is_infectious = True
-        return self._evaluate_transition()
+        self.is_infectious = False
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
     def p_death(self):
         self.is_infectious = False
-        return self._evaluate_transition()
+        self.next_state, self.steps_remaining = self._evaluate_transition()
 
 
 class Region:
@@ -251,12 +311,12 @@ class Region:
         self._population = population
         self._death = 0
         self._simulations = 0
-        self.individuals = []
+        self._individuals = []
 
         for i in range(initial_infected):
-            p = Person(self, 30)
-            p.state = StatePerson.Lp
-            self.individuals.append(p)
+            p = Person(self, 30, random.choice(['MALE', 'FEMALE']))
+            p.set_state(StatePerson.S)
+            self._individuals.append(p)
 
     @property
     def population(self):
@@ -265,6 +325,16 @@ class Region:
     @property
     def recovered(self):
         return self._recovered
+
+    def __iter__(self):
+        for i in list(self._individuals):
+            if i.state != StatePerson.S:
+                yield i
+
+    def spawn(self, age) -> Person:
+        p = Person(self, age, random.choice(['MALE', 'FEMALE']))
+        self._individuals.append(p)
+        return p
 
     def increase_susceptibles(self, count):
         """Incrementa la cantidad de personas qeu pasan a formar parte de la simulación
@@ -282,26 +352,12 @@ class Region:
         self._recovered += count
     
 
-@st.cache
-def load_interaction_estimates():
-    # no funciona, error "Could not find a version that satisfies the requirement xldr"
-    df = pd.read_excel(
-        "/src/data/contact_matrices_152_countries/MUestimates_all_locations_1.xlsx",
-        sheet_name=None,
-    )
-
-    return df
-
-
 def main():
     st.title("Simulación de la epidemia")
 
-    region = Region(1000, 1)
-
-    spatial_transmision([region], None, None, None, None)
-
-    st.write("---")
-    st.button("Ejecutar de nuevo")
+    if st.button("Simular"):
+        region = Region(1000, 1)
+        spatial_transmision([region], load_interaction_estimates(), None, None, None)
 
 
 if __name__ == "__main__":
